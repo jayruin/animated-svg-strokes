093a4d8cac5fd8e88b384e217a97b11d783718b1
jayruin <28711665+jayruin@users.noreply.github.com>
2022-12-02 17:48:17 -0500

    Cleanup old js

--- convert.js
+++
@@ -1,5 +0,0 @@
-export function svgToImg(svg) {
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(new Blob([new XMLSerializer().serializeToString(svg)], { type: "image/svg+xml" }));
-    return img;
-}
