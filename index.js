1ef64bcc6da2773173a13cb0c1da13eff546e689
jayruin <28711665+jayruin@users.noreply.github.com>
2023-12-28 13:21:21 -0500

    Use dynamic sources and formats

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // Importing from github directly will fail due to incorrect mimetype of text/plain
 // import { strokes } from "https://raw.githubusercontent.com/jayruin/strokes/dist/index.js";
-import { strokes } from "https://cdn.jsdelivr.net/gh/jayruin/strokes@dist/strokes.js";
+import { strokes, getSources, getFormats } from "https://cdn.jsdelivr.net/gh/jayruin/strokes@dist/strokes.js";
+
+const sources = Array.from(getSources()).reverse();
+const formats = Array.from(getFormats()).reverse();
 
 const animationOptions = { totalStrokeDuration: 0.5 };
 
 const characterInput = document.getElementById("character-input");
-const zhChecked = document.getElementById("zh-checked");
-const jaChecked = document.getElementById("ja-checked");
-const strokesOutputFormat = document.getElementById("strokes-output-format");
-
-const zhTarget = document.getElementById("target-zh");
-const jaTarget = document.getElementById("target-ja");
-
-const zhExistingCharacters = new Set();
-const jaExistingCharacters = new Set();
 
 const renderButton = document.getElementById("render-button");
 const clearButton = document.getElementById("clear-button");
@@ -24,15 +18,67 @@ function clear(element) {
     }
 }
 
+const existingCharacters = new Map();
+
+const sourcesCheckboxes = document.getElementById("sources-checkboxes");
+const sourcesOutputs = document.getElementById("sources-outputs");
+function addSourceCheckbox(source) {
+    const flexItem = document.createElement("div");
+    flexItem.classList.add("flex-item");
+    sourcesCheckboxes.append(flexItem);
+    const label = document.createElement("label");
+    label.classList.add("options");
+    flexItem.append(label);
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = true;
+    input.id = `${source}-checked`;
+    label.append(input);
+    const text = document.createElement("div");
+    text.append(source);
+    label.append(text);
+}
+function addSourceOutputs(source) {
+    const flexItem = document.createElement("div");
+    flexItem.classList.add("flex-item");
+    sourcesOutputs.append(flexItem);
+    const output = document.createElement("div");
+    output.id = `${source}-outputs`;
+    output.classList.add("strokes-container");
+    output.classList.add("flex-column");
+    output.classList.add("flex-center");
+    flexItem.append(output);
+}
+function addSource(source) {
+    addSourceCheckbox(source);
+    addSourceOutputs(source)
+}
+
+for (const source of sources) {
+    existingCharacters.set(source, new Set());
+    addSource(source);
+}
+
+const strokesFormat = document.getElementById("strokes-format");
+function addFormat(format) {
+    const option = document.createElement("option");
+    option.value = format;
+    option.append(format);
+    strokesFormat.append(option);
+}
+for (const format of formats) {
+    addFormat(format);
+}
+
 let draggedElement = null;
-function renderCharacter(character, characterType, characterStrokes, target, existingCharacters) {
+function renderCharacter(character, source, characterStrokes) {
     if (characterStrokes === null) {
         return;
     }
     characterStrokes.classList.add("character-strokes");
     const characterStrokesContainer = document.createElement("div");
     characterStrokesContainer.draggable = true;
-    characterStrokesContainer.id = `character-strokes-${characterType}-${character}`;
+    characterStrokesContainer.id = `character-strokes-${source}-${character}`;
     characterStrokesContainer.classList.add("character-strokes-container");
     characterStrokesContainer.append(characterStrokes);
     characterStrokesContainer.addEventListener("click", event => {
@@ -45,11 +91,11 @@ function renderCharacter(character, characterType, characterStrokes, target, exi
         const targetIdParts = event.target.id.split('-');
         const draggedIdValid = draggedIdParts.length === 4 && draggedIdParts[0] === "character" && draggedIdParts[1] === "strokes";
         const targetIdValid = targetIdParts.length === 4 && targetIdParts[0] === "character" && targetIdParts[1] === "strokes";
-        const draggedCharacterType = draggedIdParts[2];
+        const draggedCharacterSource = draggedIdParts[2];
         const draggedCharacter = draggedIdParts[3];
-        const targetCharacterType = targetIdParts[2];
+        const targetCharacterSource = targetIdParts[2];
         const targetCharacter = targetIdParts[3];
-        if (!draggedIdValid || !targetIdValid || draggedCharacterType !== targetCharacterType || draggedCharacter === targetCharacter) {
+        if (!draggedIdValid || !targetIdValid || draggedCharacterSource !== targetCharacterSource || draggedCharacter === targetCharacter) {
             return;
         }
         event.preventDefault();
@@ -61,30 +107,32 @@ function renderCharacter(character, characterType, characterStrokes, target, exi
         }
     });
     characterStrokesContainer.addEventListener("dragend", () => draggedElement = null);
-    target.prepend(characterStrokesContainer);
-    existingCharacters.add(character);
+    document.getElementById(`${source}-outputs`).prepend(characterStrokesContainer);
+    existingCharacters.get(source).add(character);
 }
 
-renderButton.addEventListener("click", async function () {
+renderButton.addEventListener("click", async () => {
     const character = characterInput.value;
-    const zhRender = zhChecked.checked && !zhExistingCharacters.has(character);
-    const jaRender = jaChecked.checked && !jaExistingCharacters.has(character);
     const promises = [];
-    promises.push(zhRender ? strokes("zh", strokesOutputFormat.value, animationOptions)(character) : Promise.resolve(null));
-    promises.push(jaRender ? strokes("ja", strokesOutputFormat.value, animationOptions)(character) : Promise.resolve(null));
-    const [zhCharacterStrokes, jaCharacterStrokes] = await Promise.all(promises);
-    renderCharacter(character, "zh", zhCharacterStrokes, zhTarget, zhExistingCharacters);
-    renderCharacter(character, "ja", jaCharacterStrokes, jaTarget, jaExistingCharacters);
+    for (const source of sources) {
+        const shouldRender = document.getElementById(`${source}-checked`).checked && !existingCharacters.get(source).has(character);
+        promises.push(shouldRender ? strokes(source, strokesFormat.value, animationOptions)(character) : Promise.resolve(null));
+    }
+    const outputs = await Promise.all(promises.values());
+    let i = 0;
+    for (const source of sources) {
+        const characterStrokes = outputs[i];
+        renderCharacter(character, source, characterStrokes);
+        i += 1;
+    }
 });
 
-clearButton.addEventListener("click", async function () {
-    if (zhChecked.checked) {
-        clear(zhTarget);
-        zhExistingCharacters.clear();
-    }
-    if (jaChecked.checked) {
-        clear(jaTarget);
-        jaExistingCharacters.clear();
+clearButton.addEventListener("click", () => {
+    for (const source of sources) {
+        if (document.getElementById(`${source}-checked`).checked) {
+            clear(document.getElementById(`${source}-outputs`));
+            existingCharacters.get(source).clear();
+        }
     }
 });
 
@@ -100,16 +148,9 @@ clearButton.addEventListener("drop", event => {
     if (!idValid) {
         return;
     }
-    const characterType = idParts[2];
+    const characterSource = idParts[2];
     const character = idParts[3];
-    switch (characterType) {
-        case "zh":
-            zhExistingCharacters.delete(character);
-            break;
-        case "ja":
-            jaExistingCharacters.delete(character);
-            break;
-    }
+    existingCharacters[characterSource].delete(character);
     draggedElement.remove();
 });
 
@@ -119,13 +160,11 @@ function renderTheme() {
     if (isDark) {
         root.style.setProperty("--color", "white");
         root.style.setProperty("--background-color", "black");
-        zhTarget.style.filter = "invert(100%)";
-        jaTarget.style.filter = "invert(100%)";
+        sourcesOutputs.style.filter = "invert(100%)";
     } else {
         root.style.setProperty("--color", "black");
         root.style.setProperty("--background-color", "white");
-        zhTarget.style.filter = "";
-        jaTarget.style.filter = "";
+        sourcesOutputs.style.filter = "";
     }
 }
 function prefersDarkSchemeQuery() {
@@ -139,7 +178,7 @@ function renderPreferredTheme() {
     }
     renderTheme();
 }
-document.getElementById("toggle-theme-button").addEventListener("click", async function () {
+document.getElementById("toggle-theme-button").addEventListener("click", () => {
     isDark = !isDark;
     renderTheme();
 });
