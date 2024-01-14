dcd3882d0396edd966474977ada5bb7c317c9abf
jayruin <28711665+jayruin@users.noreply.github.com>
2024-01-13 22:08:17 -0500

    Add runtime type validation for characterString

--- src/characters/code-point.ts
+++ src/characters/code-point.ts
@@ -1,4 +1,7 @@
 export const getCodePoint = (characterString: string): number => {
+    if (typeof characterString !== "string") {
+        throw new TypeError("characterString must be a string.");
+    }
     if (characterString.length !== 1) {
         throw new RangeError("Must be a single character.");
     }
