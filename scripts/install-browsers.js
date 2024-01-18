3e8652c2cc47d928bd72aacf4c1be374c50ffcd3
jayruin <28711665+jayruin@users.noreply.github.com>
2024-01-17 19:55:31 -0500

    Attempt to fix github actions

--- scripts/install-browsers.js
+++ scripts/install-browsers.js
@@ -1,3 +1,3 @@
 import { spawn } from "child_process";
 
-spawn("playwright", ["install", "--with-deps", "chromium"], { env: { PLAYWRIGHT_BROWSERS_PATH: 0 }, stdio: "inherit", shell: true });
+spawn("npx", ["playwright", "install", "--with-deps", "chromium"], { env: { PLAYWRIGHT_BROWSERS_PATH: 0 }, stdio: "inherit", shell: true });
--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,3 +1,3 @@
 import { spawn } from "child_process";
 
-spawn("vitest", ["run"], { env: { PLAYWRIGHT_BROWSERS_PATH: 0 }, stdio: "inherit", shell: true });
+spawn("npx", ["vitest", "run"], { env: { PLAYWRIGHT_BROWSERS_PATH: 0 }, stdio: "inherit", shell: true });
