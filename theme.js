f7b31bf123699d6fa5cfa0fa4936a4927ad07f4d
jayruin <28711665+jayruin@users.noreply.github.com>
2024-01-27 17:22:45 -0500

    Snapshot theme

--- theme.js
+++ theme.js
@@ -1,5 +1,7 @@
 import { getAllSources, getOutputsContainer } from "./sources.js";
+import { searchParams } from "./url-snapshot.js";
 
+const searchParamKey = "theme";
 
 let isDark = false;
 
@@ -23,18 +25,23 @@ function renderTheme() {
     root.style.setProperty("--background-color", backgroundColor);
     getAllSources().map(source => getOutputsContainer(source)).forEach(container => container.style.filter = filter);
     document.querySelectorAll("img").forEach(i => i.style.filter = filter);
+    searchParams.set(searchParamKey, isDark ? "dark" : "light");
 }
 
 function prefersDarkSchemeQuery() {
     return window.matchMedia("(prefers-color-scheme: dark)");
 }
 
-function renderPreferredTheme() {
+function setPreferredTheme() {
     if (prefersDarkSchemeQuery().matches) {
         isDark = true;
     } else {
         isDark = false;
     }
+}
+
+function renderPreferredTheme() {
+    setPreferredTheme();
     renderTheme();
 }
 
@@ -46,5 +53,15 @@ export function setupTheme(toggleThemeButton) {
 
     prefersDarkSchemeQuery().addEventListener("change", renderPreferredTheme);
 
-    renderPreferredTheme();
+    const searchParamValue = searchParams.get(searchParamKey);
+
+    if (searchParamValue === "dark") {
+        isDark = true;
+    }  else if (searchParamValue === "light") {
+        isDark = false;
+    } else {
+        setPreferredTheme();
+    }
+
+    renderTheme();
 }
